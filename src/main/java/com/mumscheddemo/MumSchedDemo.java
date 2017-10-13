627ff2ab987aa7cd9ac633b3208f9ea4716ad57a
Binesh Sah <binesh.sah@gmail.com>
2017-10-13 14:42:29 -0500

    Changes

---
+++ src/main/java/com/mumscheddemo/MumSchedDemo.java
@@ -0,0 +1,12 @@
+package com.mumscheddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumSchedDemo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MumSchedDemo.class, args);
+	}
+}
